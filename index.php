0039ac90a50f35175a6d1c9a0fb197db83e8f757
marc0tjevp <marco.van.poortvliet@trust.com>
2017-01-06 14:20:18 +0100

    Minor changes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Portfolio - Marco van Poortvliet</title>
+    <title>Marco van Poortvliet</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
     <link type="text/css" rel="stylesheet" href="css/style.css" />
